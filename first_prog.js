c62067cb71059380db5f755467ed38c4c8cb6ca3
Riya <riyahati01@gmail.com>
2023-08-27 02:48:43 +0530

    upto operators

---
+++ first_prog.js
@@ -0,0 +1,132 @@
+// console.log('Hello From Riya Hati');
+// alert('hello')
+
+const laptop={
+    model:'xxx',
+    Color:'white'
+}
+
+console.log(laptop);
+
+//variable declaration. It's 3 types--- Var,Let,Const---Var is old.
+
+let language='JavaScript';
+console.log(language);
+
+
+//block scope
+if(true){
+    let age=25;
+    console.log('age');
+}
+
+//data types
+
+let age=25;
+age='twinty five';
+console.log(age);
+
+//A.primitive data types
+//1.Number
+// let age=25;
+// let temp=-25;
+// let price=50.50;
+
+// 2.String
+// let name='riya';
+
+// 3.Boolean
+// true/false
+// 4.Undefined
+// let age;
+// console.log(age);
+// 5.BigInt
+// 6.Symbol
+// unique
+
+// B)Structural Type
+// 1.Object
+// 1.1 function
+//    Non data structure
+//    callable
+// 1.2 Arrays
+//     collection of values
+//     const numbers=[1,2,4,5,'js',{ name:'riya'}]
+    // 1.3 Maps
+    // 1.4 Set
+    //1.5 Date
+//c. Structural root
+    // 1.1 null
+
+
+
+
+
+    //operators
+
+    /*
+    1.Assignment Operators
+    2.Comparision Operators
+    3.Arithmetic Operators
+    4.Bitwise Operators
+    5.Logical Operators
+    6.String Operators
+    7.Conditional Operators
+    8.Comma Operators
+    9.Unary Operators
+    10.Relational Operators 
+    */
+
+    //1.Assignment Operators
+    // x=20;
+    // y=10;
+    // x=y;
+    // console.log(x);
+    // //compound operators
+    // x=20;
+    // y=10;
+    // x+=y;      
+    // console.log(x+y);
+
+//Logical Operators
+//1. LOgical AND &&
+//   let happy=true && true;
+//   console.log(happy);
+
+
+// const isLoggedIn=true;
+// const hasPermissions=true;
+//  if(isLoggedIn && hasPermissions) {
+//     //Delete an item
+//     console.log('item deleted succesfully');
+//  }
+
+ //Logical OR ||
+ 
+// const isLoggedIn=true;
+// const hasPermissions=true;
+//  if(isLoggedIn || hasPermissions) {
+//     //Delete an item
+//     console.log('item deleted succesfully');
+//  }
+
+//Logical NOT !
+// const happy=true;
+// console.log(!happy);
+
+//Conditional Operators
+
+// const userRole= 'admin';
+// if(userRole==='admin'){
+//     console.log('U r a admin');
+// }
+// else{
+//     console.log('u r not a admin');
+// }
+
+//syntax of conditional operators
+// condition ? '' : '';
+// const userRole='admin';
+// userRole === 'admin' ? console.log('U r a admin'):  console.log('u r not an admin');
+
+//comparision operator
